dd634110073ff86351b6111ae697db583d69b500
Narine Voskanyan <narine.voskanyan@netsoft-usa.com>
2012-12-28 17:38:57 +0400

    Test dir deleted

---
+++ doc/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EMS::Application.config.secret_token = 'e30d621180aa56e820902fe8e1b61e6944d926af36b99edb954183a6af2a8358387b1b438b3a759976440a53c3a76bfef70366a9887cdc9513203e20fa0cc7c1'
